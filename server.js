3ee082454bb31a84bde46968d70596c2016478bd
vanhoa690 <vanhoa690@gmail.com>
2025-01-09 09:05:30 +0700

    done ca 1

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
+// Middleware để phân tích JSON và form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.send("Hello NodeJS");
 });
 
 app.get("/products", function (req, res) {
+  // console.log("query", req.query);
+  const priceMin = req.query.priceMin;
+  const priceMax = req.query.priceMax;
   //data product list
   const products = [
     {
       name: "Iphone",
-      price: "111",
+      price: 99,
       id: 1,
     },
     {
       name: "laptop",
-      price: "111",
+      price: 111,
       id: 2,
     },
   ];
+
+  if (priceMin && priceMax) {
+    const productFilter = products.filter(
+      (product) => product.price > priceMin && product.price < priceMax
+    );
+    return res.json(productFilter);
+  }
   res.json(products);
 });
 
@@ -45,6 +61,11 @@ app.get("/products/:id", function (req, res) {
     return res.status(404).json({ message: "Product Not Found" });
   }
   res.json(product);
+});
+
+app.post("/products", function (req, res) {
+  console.log("req body", req.body);
+  res.send("Hello Add Product");
 });
 
 app.get("/login", function (req, res) {
