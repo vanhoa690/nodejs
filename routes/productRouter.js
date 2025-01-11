cb5479ffd267f64c11f1c14a42c460094f0c65a9
vanhoa690 <vanhoa690@gmail.com>
2025-01-11 08:05:04 +0700

    refactor

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,20 +4,60 @@ const productRouter = Router();
 //5 routes RESTfull API
 
 productRouter.get("/", function (req, res) {
-  res.send("Product List");
+  const query = req.query;
+  const priceMin = req.query.priceMin;
+  const priceMax = req.query.priceMax;
+  //data product list
+  const products = [
+    {
+      name: "laptop",
+      price: 11,
+      id: 1,
+    },
+    {
+      name: "laptop",
+      price: 999,
+      id: 2,
+    },
+  ];
+  if (priceMin && priceMax) {
+    const productFilter = products.filter(
+      (product) => product.price > priceMin && product.price < priceMax
+    );
+    return res.status(200).json(productFilter);
+  }
+  res.status(200).json(products);
 });
-
 productRouter.get("/:id", function (req, res) {
-  res.send("Product Detail");
+  const id = req.params.id;
+  //data product list
+  const products = [
+    {
+      name: "laptop",
+      price: 1,
+      id: 1,
+    },
+    {
+      name: "laptop",
+      price: 99,
+      id: 2,
+    },
+  ];
+  const product = products.find((product) => product.id == id);
+  if (!product) {
+    return res.status(404).json({ message: "Product Not Found" });
+  }
+  res.status(200).json(product);
 });
 productRouter.post("/", function (req, res) {
-  res.send("Product Add");
+  console.log("req body", req.body);
+  res.send("Hello Product Add");
 });
 productRouter.put("/:id", function (req, res) {
-  res.send("Product Update");
+  res.send("Hello Product Update");
 });
 productRouter.delete("/:id", function (req, res) {
-  res.send("Product Delete");
+  res.send("Hello Product Delete");
 });
 
 export default productRouter;
