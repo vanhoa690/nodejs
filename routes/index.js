2e9c13fd5064efbfd32db08e0b29ddf1f5008946
vanhoa690 <vanhoa690@gmail.com>
2025-01-11 08:02:08 +0700

    update productRouter

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import productRouter from "./productRouter";
+
+const router = Router();
+
+router.use("/products", productRouter);
+
+export default router;
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,20 +3,20 @@ import { Router } from "express";
 const productRouter = Router();
 //5 routes RESTfull API
 
-productRouter.get("/products", function (req, res) {
+productRouter.get("/", function (req, res) {
   res.send("Product List");
 });
 
-productRouter.get("/products/:id", function (req, res) {
+productRouter.get("/:id", function (req, res) {
   res.send("Product Detail");
 });
-productRouter.post("/products", function (req, res) {
+productRouter.post("/", function (req, res) {
   res.send("Product Add");
 });
-productRouter.put("/products/:id", function (req, res) {
+productRouter.put("/:id", function (req, res) {
   res.send("Product Update");
 });
-productRouter.delete("/products/:id", function (req, res) {
+productRouter.delete("/:id", function (req, res) {
   res.send("Product Delete");
 });
 
