a774bfd7f8430a6b810665242279609ff041fb06
vanhoa690 <vanhoa690@gmail.com>
2025-01-11 08:11:48 +0700

    authRouter

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import productRouter from "./productRouter";
+import authRouter from "./authRouter";
 
 const router = Router();
 
 router.use("/products", productRouter);
+router.use("/auth", authRouter);
 
 export default router;
