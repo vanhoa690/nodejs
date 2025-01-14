d430b1da5de6a19766cde3aabbcdfe8e7043cfd5
vanhoa690 <vanhoa690@gmail.com>
2025-01-14 08:42:20 +0700

    update product

---
+++ controllers/productController.js
@@ -0,0 +1,64 @@
+// Demo Mock data (sử dụng nếu chưa có database)
+
+const products = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+];
+
+function getAllProducts(req, res) {
+  res.json(products);
+}
+
+function getProductDetail(req, res) {
+  const { id } = req.params;
+  const product = products.find((p) => p.id == id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product Not Found" });
+  }
+  res.json(product);
+}
+
+function createProduct(req, res) {
+  const { name, price } = req.body;
+
+  if (!name || !price) {
+    return res.status(400).json({ message: "Name and Price is required" });
+  }
+  const newProduct = {
+    id: products.length + 1,
+    name,
+    price,
+  };
+  products.push(newProduct);
+
+  res.json(newProduct);
+}
+
+function updateProduct(req, res) {
+  const { id } = req.params;
+  const product = products.find((p) => p.id == id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product Not Found" });
+  }
+
+  const { name, price } = req.body;
+
+  if (name) product.name = name;
+  if (price) product.price = price;
+
+  res.json(product);
+}
+
+function deleteProduct(req, res) {
+  res.send("deleteProduct");
+}
+
+export {
+  getAllProducts,
+  getProductDetail,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,63 +1,18 @@
 import { Router } from "express";
+import {
+  getAllProducts,
+  getProductDetail,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController";
 
 const productRouter = Router();
 
-productRouter.get("/", function (req, res) {
-  const query = req.query;
-  const priceMin = req.query.priceMin;
-  const priceMax = req.query.priceMax;
-  //data product list
-  const products = [
-    {
-      name: "laptop",
-      price: 11,
-      id: 1,
-    },
-    {
-      name: "laptop",
-      price: 999,
-      id: 2,
-    },
-  ];
-  if (priceMin && priceMax) {
-    const productFilter = products.filter(
-      (product) => product.price > priceMin && product.price < priceMax
-    );
-    return res.status(200).json(productFilter);
-  }
-  res.status(200).json(products);
-});
-productRouter.get("/:id", function (req, res) {
-  const id = req.params.id;
-  //data product list
-  const products = [
-    {
-      name: "laptop",
-      price: 1,
-      id: 1,
-    },
-    {
-      name: "laptop",
-      price: 99,
-      id: 2,
-    },
-  ];
-  const product = products.find((product) => product.id == id);
-  if (!product) {
-    return res.status(404).json({ message: "Product Not Found" });
-  }
-  res.status(200).json(product);
-});
-productRouter.post("/", function (req, res) {
-  console.log("req body", req.body);
-  const data = req.body;
-  res.json(data);
-});
-productRouter.put("/:id", function (req, res) {
-  res.send("Hello Product Update");
-});
-productRouter.delete("/:id", function (req, res) {
-  res.send("Hello Product Delete");
-});
+productRouter.get("/", getAllProducts);
+productRouter.get("/:id", getProductDetail);
+productRouter.post("/", createProduct);
+productRouter.put("/:id", updateProduct);
+productRouter.delete("/:id", deleteProduct);
 
 export default productRouter;
