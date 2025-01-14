9053662c0463ec6f37faada4a92d2abc9dcda21b
vanhoa690 <vanhoa690@gmail.com>
2025-01-14 07:20:10 +0700

    convert ca 5

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes";
-import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
@@ -9,18 +8,6 @@ app.use(cors());
 // Middleware để phân tích JSON và form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-async function connectMongoDB() {
-  try {
-    //mongodb://127.0.0.1:27017/db_name
-    await mongoose.connect("mongodb://127.0.0.1:27017/db_node_sp25");
-    console.log("Connect successfully!!!");
-  } catch (error) {
-    console.log("Connect failure!!!");
-  }
-}
-
-connectMongoDB();
 
 app.use("/", router);
 
