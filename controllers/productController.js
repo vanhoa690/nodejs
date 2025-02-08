1aa48c770458bd75d398306c06f53481f82b49a6
vanhoa690 <vanhoa690@gmail.com>
2025-02-08 07:19:36 +0700

    convert db

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,36 +1,20 @@
 // Demo Mock data (sử dụng nếu chưa có database)
-
-import productModel from "../models/productModel";
-
 const products = [
   { id: 1, name: "Product 1", price: 100 },
   { id: 2, name: "Product 2", price: 200 },
 ];
 
-async function getAllProducts(req, res) {
-  try {
-    const productList = await productModel.find(); // SQL: select * from products
-    res.json(productList);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: "Server Error" });
-  }
+function getAllProducts(req, res) {
+  res.json(products);
 }
 
-async function getProductDetail(req, res) {
-  try {
-    const { id } = req.params;
-
-    const product = await productModel.findById(id); //SQL: select * form products where product.id === id
-
-    if (!product) {
-      return res.status(404).json({ message: "Product Not Found" });
-    }
-    res.json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: "Server Error" });
+function getProductDetail(req, res) {
+  const { id } = req.params;
+  const product = products.find((product) => product.id == id);
+  if (!product) {
+    return res.status(404).json({ message: "Product Not Found" });
   }
+  res.json(product);
 }
 
 function createProduct(req, res) {
@@ -66,23 +50,15 @@ function updateProduct(req, res) {
   res.json(product);
 }
 
-async function deleteProduct(req, res) {
-  try {
-    const { id } = req.params;
-
-    const deleteProduct = await productModel.findByIdAndDelete(id); //find(id) + delete Product
-
-    console.log(deleteProduct);
-
-    if (!deleteProduct) {
-      return res.status(404).json({ message: "Product Not Found" });
-    }
-
-    res.json({ message: "Xoa san pham thanh cong" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: "Server Error" });
+function deleteProduct(req, res) {
+  const { id } = req.params;
+  const productIndex = products.findIndex((p) => p.id == id);
+  console.log({ productIndex });
+  if (productIndex == -1) {
+    return res.staus(404).json({ message: "Product Not Found" });
   }
+  products.splice(productIndex, 1);
+  res.json({ message: "Xoa san pham thanh cong" });
 }
 
 export {
