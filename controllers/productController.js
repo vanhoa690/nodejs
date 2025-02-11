62b30be4992a0923fe42885a66c273c5171c9030
vanhoa690 <vanhoa690@gmail.com>
2025-02-11 09:49:42 +0700

    remove products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,5 @@
 import productModel from "../models/productModel";
 
-// Demo Mock data (sử dụng nếu chưa có database)
-const products = [
-  { id: 1, name: "Product 1", price: 100 },
-  { id: 2, name: "Product 2", price: 200 },
-];
-
 async function getAllProducts(req, res) {
   try {
     const productList = await productModel.find();
