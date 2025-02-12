f3d8df072b076aee3b0dda5cb4da2573637f7ebf
vanhoa690 <vanhoa690@gmail.com>
2025-02-12 17:33:11 +0700

    userModel

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
