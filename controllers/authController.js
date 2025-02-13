ef3bab1efe1d68d26ef364cd59e70ead873fd098
vanhoa690 <vanhoa690@gmail.com>
2025-02-13 08:22:52 +0700

    add user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,32 @@
-function register(req, res) {
-  res.json("API Register");
+import userModel from "../models/userModel";
+
+async function register(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // Kiem tra du lieu hop le: joi, express-validator
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and Password is Required" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password min 6 character" });
+    }
+
+    // them user
+    const newUser = {
+      email,
+      password,
+    };
+
+    const userCreated = await userModel.create(newUser);
+
+    res.json(userCreated);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 export { register };
