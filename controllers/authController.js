3ce84c30429cfc1de15e68b0c5c3e1acf2fedc16
vanhoa690 <vanhoa690@gmail.com>
2025-02-13 10:20:46 +0700

    add user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
-function register(req, res) {
-  res.json("API Register");
+async function register(req, res) {
+  try {
+    const { email, password } = req.body;
+    //Kiem tra du lieu hop le
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and Password is Required" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password min 6 character" });
+    }
+
+    // them user
+    const newUser = {
+      email,
+      password,
+    };
+    const userCreated = await userModel.create(newUser);
+    res.json(userCreated);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 export { register };
