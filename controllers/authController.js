bb7eedf8b4ce4386fbbd99a8b50f540c824a461b
vanhoa690 <vanhoa690@gmail.com>
2025-02-13 10:29:57 +0700

    hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+import userModel from "../models/userModel";
+import bcrypt from "bcryptjs";
+
 async function register(req, res) {
   try {
     const { email, password } = req.body;
@@ -11,10 +14,14 @@ async function register(req, res) {
       return res.status(400).json({ message: "Password min 6 character" });
     }
 
+    // Mã hóa mật khẩu
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
     // them user
     const newUser = {
       email,
-      password,
+      password: hashedPassword,
     };
     const userCreated = await userModel.create(newUser);
     res.json(userCreated);
