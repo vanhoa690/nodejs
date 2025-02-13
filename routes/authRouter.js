969537151ef257c1ea59d68f6410b802313c474a
vanhoa690 <vanhoa690@gmail.com>
2025-02-13 07:58:35 +0700

    auth route regiter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { register } from "../controllers/authController";
 
 const authRouter = Router();
+
+authRouter.post("/register", register);
 
 export default authRouter;
