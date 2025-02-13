5a156b12dbe792ada2ac7c41f1a591f46ab258ad
vanhoa690 <vanhoa690@gmail.com>
2025-02-13 09:25:29 +0700

    convert ca 1

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import { register } from "../controllers/authController";
 
 const authRouter = Router();
-
-authRouter.post("/register", register);
 
 export default authRouter;
