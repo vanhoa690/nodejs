7e96a5cb8542ee8a7cb164844de0a57d46ab7cbb
vanhoa690 <vanhoa690@gmail.com>
2025-02-20 10:40:02 +0700

    validate register

--- validation/auth.js
+++ validation/auth.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  username: Joi.string().optional(),
+  //   username: Joi.string().optional(),
 });
 
 export { registerSchema };
